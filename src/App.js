2501102d2015f6b1ff41f76e9aaa3fcec63a45b4
Anatoliy Vershinin <cata07@yandex.ru>
2018-04-03 09:04:04 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { PureComponent } from 'react';
+import './App.css';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>Hello</h2>
+      </div>
+    );
+  }
+}
+
+export default App;
