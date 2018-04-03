a8be574e780504aa8e93f985899130bacec57017
a.vershinin <a.vershinin@nixsolution.com>
2018-04-03 17:33:42 +0300

    change connect store & middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { ConnectedRouter } from 'react-router-redux';
-import store, { history } from "./store/index";
+import createHistory from 'history/createBrowserHistory';
+
+import configureStore from "./store/index";
 
 import App from './App';
 
+const history = createHistory();
+const initialState = {};
+
+/* Создаём историю для переходов. Используй роутинг через browser history*/
+const store = configureStore(initialState, history);
 
 
 /* ConnectedRouter связывает стор и роуты автоматически*/
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,31 @@
 import { createStore, applyMiddleware  } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import createHistory from 'history/createBrowserHistory';
 import thunk from 'redux-thunk';
+import ReduxThunk from 'redux-thunk';
 import rootReducer from '../reducers/rootReducer.js';
 
-/* Создаём историю для переходов. Используй роутинг через browser history*/
-export const history = createHistory();
 
-/* Создаём мидлвары. Прокидываем в мидлвары history переходов */
-const middleware = routerMiddleware(history);
-
-const store = createStore(rootReducer, composeWithDevTools(
-	applyMiddleware(middleware)
-));
+/* Создаём мидлвары. Связываем роутерМидлвар и history для переходов */
 
 
-export default store;
+const configureStore = (initialState, history) => {
+	const middlewares = [
+			thunk,
+			routerMiddleware(history),
+	];
+
+	const store = createStore(
+	  rootReducer,
+	  initialState,
+	  composeWithDevTools(
+	     applyMiddleware(...middlewares)
+	  ),
+	);
+
+
+	return store;
+}
+
+
+export default configureStore;
