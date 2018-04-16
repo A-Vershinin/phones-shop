f6899e46533f9f5c550587c3eba4c3c481f72d6d
a.vershinin <a.vershinin@nixsolution.com>
2018-04-16 18:28:58 +0300

    chenge phones actions

--- src/actions/phonesAction.js
+++ src/actions/phonesAction.js
@@ -5,72 +5,100 @@ import {
   fetchPhones as fetchPhonesApi, loadMorePhones as loadMorePhonesApi, fetchPhoneById as fetchPhoneByIdApi,
 } from '../api/fetchPhonesApi';
 
-import { getPhones, getRenderedPhonesLength} from '../selectors/selectors';
+import { getPhones, getRenderedPhonesLength } from '../selectors/selectors';
+
+export const fetchPhonesStartAction= () => ({
+  type: FETCH_PHONES_START
+});
+
+export const fetchPhonesSuccesPhonesAction= (phones) => ({
+  type: FETCH_PHONES_SUCCESS,
+  payload: phones
+});
+
+export const fetchPhonesSuccesDataAction= (data) => ({
+  type: FETCH_PHONES_SUCCESS,
+  payload: data
+});
+
+export const fetchPhonesFailureAction= (err) => ({
+  type: FETCH_PHONES_FAILURE,
+  payload: err,
+  error: true
+});
 
 export const fetchPhonesAction = () => async (dispatch, getState) => {
-  dispatch({type: FETCH_PHONES_START});
+  dispatch(fetchPhonesStartAction());
   try {
     const phones = await fetchPhonesApi();
 
-    dispatch({
-      type: FETCH_PHONES_SUCCESS,
-      payload: phones
-    });
+    dispatch(fetchPhonesSuccesPhonesAction(phones));
 
     /* получили объекты из стора*/
     const data = getPhones(getState());
 
-    dispatch({
-      type: FETCH_PHONES_SUCCESS,
-      payload: data
-    });
+    dispatch(fetchPhonesSuccesDataAction(data));
 
   } catch (err) {
-    dispatch({
-      type: FETCH_PHONES_FAILURE,
-      payload: err,
-      error: true
-    });
+    dispatch(fetchPhonesFailureAction(err));
   }
 };
+ /* -----------------------------------------------------------------------------*/
 
+export const loadMorePhonesStartAction = () => ({
+  type: LOAD_MORE_PHONES_START
+});
+
+
+export const loadMorePhonesSuccessAction = (phones) => ({
+  type: LOAD_MORE_PHONES_SUCCESS,
+  payload: phones
+});
+
+export const loadMorePhonesFailureAction = (err) => ({
+  type: LOAD_MORE_PHONES_FAILURE,
+  payload: err,
+  error: true
+});
 
 export const loadMorePhonesAction = () => async (dispatch, getState) => {
-  const offset = getRenderedPhonesLength(getState())
-  dispatch({type: LOAD_MORE_PHONES_START})
+  const offset = getRenderedPhonesLength(getState());
+  dispatch(loadMorePhonesStartAction());
 
   try {
     const phones = await loadMorePhonesApi(offset)
+    dispatch(loadMorePhonesSuccessAction(phones));
 
-    dispatch({
-      type: LOAD_MORE_PHONES_SUCCESS,
-      payload: phones
-    })
   } catch (err) {
-    dispatch({
-      type: LOAD_MORE_PHONES_FAILURE,
-      payload: err,
-      error: true
-    })
+    dispatch(loadMorePhonesFailureAction(err));
   }
 }
+ /* -----------------------------------------------------------------------------*/
+
+export const fetchPhoneByIdStart = () => ({
+  type: FETCH_PHONE_BY_ID_START
+});
+
+export const fetchPhoneByIdSuccess = (phone) => ({
+  type: FETCH_PHONE_BY_ID_SUCCESS,
+  payload: phone
+});
+
+export const fetchPhoneByIdFailure = (err) => ({
+  type: FETCH_PHONE_BY_ID_FAILURE,
+  payload: err,
+  error: true
+});
 
 
 export const fetchPhoneById = (id) => async dispatch => {
-  dispatch({type: FETCH_PHONE_BY_ID_START})
+  dispatch(fetchPhoneByIdStart());
 
   try {
-    const phone = await fetchPhoneByIdApi(id)
+    const phone = await fetchPhoneByIdApi(id);
+    dispatch(fetchPhoneByIdSuccess(phone));
 
-    dispatch({
-      type: FETCH_PHONE_BY_ID_SUCCESS,
-      payload: phone
-    })
   } catch (err) {
-    dispatch({
-      type: FETCH_PHONE_BY_ID_FAILURE,
-      payload: err,
-      error: true
-    })
+    dispatch(fetchPhoneByIdFailure(err))
   }
 }
